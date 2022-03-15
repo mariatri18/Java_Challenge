a495b7bafc299d6d0bbbe18c9ac31478c8cf35d7
Maria Tri <mariatri13@gmail.com>
2022-03-15 20:48:17 +0200

    main

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    
+        public static void main (){
+            int a = 5;
+            int b = 6;
+    
+            int d = a+b;
+            System.out.println(d);
+        }
+    }
