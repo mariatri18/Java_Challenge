d4d86be3ae2a78c6e0ea557c56c2eb5e99ea458c
Maria Tri <mariatri13@gmail.com>
2022-03-15 20:45:59 +0200

    trying some variables

--- Calculator.java
+++ Calculator.java
@@ -1,5 +1,9 @@
 public class Calculator {
-    public static void main (String args[]){
-        System.out.println("3+2");
+    public static void main (){
+        int a = 5;
+        int b = 6;
+
+        int d = a+b;
+        System.out.println(d);
     }
 }
--- Hello_World.java
+++ Hello_World.java
@@ -2,4 +2,13 @@ class Simple{
     public static void main (String args[]){
         System.out.println("Hello Java");
     }
+
+    public static void main (){
+        int a = 5;
+        int b = 6;
+
+        int d = a+b;
+        System.out.println(d);
+    }
 }
+
