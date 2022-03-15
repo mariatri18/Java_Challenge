dde7cdac7ff889926ffad168bca8cbfd8917b1b9
Maria Tri <mariatri13@gmail.com>
2022-03-15 20:52:37 +0200

    Changes in main_class & Calcuator

--- Calculator.java
+++ Calculator.java
@@ -1,9 +1,20 @@
 public class Calculator {
-    public static void main (){
+    public static void main (String args[]){
+
+        System.out.println("Calculator");
         int a = 5;
         int b = 6;
 
-        int d = a+b;
-        System.out.println(d);
+        int d1 = a+b;
+        System.out.println(d1);
+
+        int d2 = a-b;
+        System.out.println(d2);
+
+        int d3 = a*b;
+        System.out.println(d3);
+
+        int d4 = a/b;
+        System.out.println(d4);
     }
 }
--- Hello_World.java
+++ Hello_World.java
@@ -1,14 +1,7 @@
 class Simple{
     public static void main (String args[]){
+        System.out.println("Hello!!!!");
         System.out.println("Hello Java");
-    }
-
-    public static void main (){
-        int a = 5;
-        int b = 6;
-
-        int d = a+b;
-        System.out.println(d);
     }
 }
 
